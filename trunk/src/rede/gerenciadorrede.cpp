eb24fe1d38eca34ef3f2f5686c63b202a4c68e23
ungerer.ricardo <ungerer.ricardo@83fcdb62-9c42-11de-825e-994487f65616>
2009-11-21 20:13:51 +0000

    commit para teste de conexao

--- trunk/src/rede/gerenciadorrede.cpp
+++ trunk/src/rede/gerenciadorrede.cpp
@@ -9,8 +9,8 @@ GerenciadorRede::GerenciadorRede( Rede::Peer* _primeiro_peer )
     this->gerenciador_conexoes = new Rede::GerenciadorConexao::GerenciadorConexao();
     this->ouvinte = new Rede::Ouvinte(this);
 
-    QObject::connect(this->ouvinte,SIGNAL(novaConn(int)),
-                     this->gerenciador_conexoes,SLOT(novaConexao(int)));
+    QObject::connect(this->ouvinte, SIGNAL(novaConn(int)),
+                     this->gerenciador_conexoes, SLOT(novaConexao(int)));
 
     if ( _primeiro_peer == NULL )
     {
@@ -33,6 +33,7 @@ GerenciadorRede::startComoServer()
 {
     Rede::RedeConfig::getInstance().meu_id = 1;
     Rede::RedeConfig::getInstance().server = true;
+    Rede::RedeConfig::getInstance().estado_atual = Rede::SERVER;
 
     if (! this->ouvinte->startListen() )
     {
@@ -44,5 +45,8 @@ void
 GerenciadorRede::buscaPorServer( Rede::Peer* _primeiro_peer )
 {
     Rede::RedeConfig::getInstance().server = false;
+    Rede::RedeConfig::getInstance().estado_atual = Rede::PROCURANDO_SERVER;
 
+    //conectar na porta 6969 e perguntar pelo servidor
+    _primeiro_peer->conectar();
 }
