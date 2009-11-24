2c31b4d9f36c992903e62b6fdcf67b8a0010af72
ungerer.ricardo <ungerer.ricardo@83fcdb62-9c42-11de-825e-994487f65616>
2009-11-24 04:36:16 +0000

    peers em server nao iniciados no start nao estavam conectados no novopeer

--- src/rede/gerenciadorrede.cpp
+++ src/rede/gerenciadorrede.cpp
@@ -226,6 +226,9 @@ GerenciadorRede::addConexaoPeerVeterano( const int& _socket_descriptor )
     Rede::Peer*
     peer = new Rede::Peer(_socket_descriptor);
 
+    QObject::connect(this,SIGNAL(novoPeer(const QString&, const int&)),
+                     peer ,SLOT(enviaNovoPeer(const QString&, const int&)));
+
     QObject::connect(peer, SIGNAL(incommingMessage(int,QString)),
                      this, SLOT(slotNovaMensagemFromPeer(int,QString)));
 
