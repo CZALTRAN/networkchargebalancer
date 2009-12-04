5c00efd8bd514f1d9ea274300eecf5ac5fb162e6
mat.pmarques <mat.pmarques@83fcdb62-9c42-11de-825e-994487f65616>
2009-12-04 05:30:21 +0000

    vai toma no cu!

---
+++ src/gerenciador_processos/gpconfig.h
@@ -0,0 +1,37 @@
+#ifndef PROCESSOSCONFIG_H
+#define PROCESSOSCONFIG_H
+
+#include <QHash>
+#include <QPair>
+#include <QProcess>
+#include "processo.h"
+
+namespace GP
+{
+    class GPConfig
+    {
+        int
+        meu_id;
+
+    public:
+        int
+        getMeuId();
+
+        static GPConfig&
+        getInstance();
+
+    private:
+
+        static GP::GPConfig
+        instance;
+
+        GPConfig();
+
+        GPConfig( GPConfig const& );
+
+        GP::GPConfig&
+        operator= ( GP::GPConfig const& );
+
+    };
+}
+#endif // PROCESSOSCONFIG_H
