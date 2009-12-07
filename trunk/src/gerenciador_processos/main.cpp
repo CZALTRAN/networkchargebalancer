09796f021c0a2432062fc5015e092718a83b1849
ungerer.ricardo <ungerer.ricardo@83fcdb62-9c42-11de-825e-994487f65616>
2009-12-07 03:07:03 +0000

    alteracoes na rede para aguardar a conexao dos sinais para depois inicializar as paradas

--- trunk/src/gerenciador_processos/main.cpp
+++ trunk/src/gerenciador_processos/main.cpp
@@ -17,27 +17,27 @@ int main(int argc, char *argv[])
 
     qDebug() << Q_FUNC_INFO << "startando processo";
 
-    gerenciador.peerNovo(4);
-    gerenciador.peerNovo(5);
-    gerenciador.peerNovo(6);
+//    gerenciador.peerNovo(3);
+//    gerenciador.peerNovo(5);
+//    gerenciador.peerNovo(6);
 
-    gerenciador.incommingMessage(4, "GP:BALANCER:STATUS_PEER:0:1");
+/*    gerenciador.incommingMessage(4, "GP:BALANCER:STATUS_PEER:0:1");
     gerenciador.incommingMessage(5, "GP:BALANCER:STATUS_PEER:0:1");
     gerenciador.incommingMessage(6, "GP:BALANCER:STATUS_PEER:0:1");
-
-    gerenciador.incommingMessage(4, "GP:LAUNCHER:START_PROCESS:456:firefox:www.sandrinha.com.br");
+*/
+ /*   gerenciador.incommingMessage(4, "GP:LAUNCHER:START_PROCESS:456:firefox:www.sandrinha.com.br");
     gerenciador.incommingMessage(4, "GP:LAUNCHER:START_PROCESS:456:firefox:www.mundocanibal.com.br");
     gerenciador.incommingMessage(4, "GP:LAUNCHER:START_PROCESS:456:firefox:www.terra.com.br");
     gerenciador.incommingMessage(4, "GP:LAUNCHER:START_PROCESS:456:firefox:www.vivo.com.br");
     gerenciador.incommingMessage(4, "GP:LAUNCHER:START_PROCESS:456:firefox:www.twitter.com");
-    gerenciador.incommingMessage(4, "GP:LAUNCHER:START_PROCESS:456:firefox:www.twitter.com");
+    gerenciador.incommingMessage(4, "GP:LAUNCHER:START_PROCESS:456:firefox:www.twitter.com");*/
 
     gerenciador.processoStart(1, "firefox", "www.google.com");
-    gerenciador.processoStart(2, "firefox", "www.youtube.com");
+/*    gerenciador.processoStart(2, "firefox", "www.youtube.com");
     gerenciador.processoStart(3, "firefox", "www.uol.com.br");
     gerenciador.processoStart(3, "firefox", "www.redtube.com");
     gerenciador.processoStart(3, "firefox", "www.tube8.com");
-
+*/
 
     return a.exec();
 }
