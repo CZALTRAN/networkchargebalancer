5b8e77dac1d4f7a814537c4a52554cf310080656
ungerer.ricardo <ungerer.ricardo@83fcdb62-9c42-11de-825e-994487f65616>
2009-12-10 13:37:37 +0000

    deu azia no commit anterior

---
+++ trunk/src/apps/xbsh/xbsh.cpp
@@ -0,0 +1,16 @@
+#include "xbsh.h"
+
+#include <QDebug>
+
+XBSh::XBSh(QObject *_parent) :
+    QObject(_parent)
+{
+
+
+}
+
+XBSh::~XBSh()
+{
+
+}
+
---
+++ trunk/src/apps/xbsh/xbsh.h
@@ -0,0 +1,23 @@
+#ifndef XBSH_H
+#define XBSH_H
+
+#include <QObject>
+
+class XBSh : public QObject
+{
+Q_OBJECT
+public:
+
+    XBSh(QObject *_parent = 0);
+
+    ~XBSh();
+
+signals:
+
+public slots:
+
+private:
+
+};
+
+#endif // XBSH_H
