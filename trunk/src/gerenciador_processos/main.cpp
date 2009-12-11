f257458f7f93932d8b6f20e32f4d95c85e755a48
mat.pmarques <mat.pmarques@83fcdb62-9c42-11de-825e-994487f65616>
2009-12-11 03:05:03 +0000

    uns bug do caralho! Porra, eu quero fritar nisso e tenho q fazer trabalhinho em Java de BD! VAI TOMA NO CUUUUUUUu!

--- trunk/src/gerenciador_processos/main.cpp
+++ trunk/src/gerenciador_processos/main.cpp
@@ -63,14 +63,14 @@ int main(int argc, char *argv[])
 //    testeGp.gerenciador.processoStart(5, "kcalc", "");
 
 /********************** teste5 *********************************/
-//    testeGp.gerenciador.processoStart(5, "ping", "192.168.1.101");
+    testeGp.gerenciador.processoStart(5, "ping", "localhost");
 
 /********************** teste6 *********************************/
 //    testeGp.gerenciador.incommingMessage(6, "GP:LAUNCHER:START_PROCESS:5:ping:192.168.1.101");
 
 /********************** teste7 *********************************/
-    testeGp.gerenciador.processoStart(14, "pikaloka", "");
-    testeGp.gerenciador.processoStart(15, "pikaloka", "");
+//    testeGp.gerenciador.processoStart(14, "pikaloka", "");
+//    testeGp.gerenciador.processoStart(15, "pikaloka", "");
 
 /********************** teste8 *********************************/
 //    testeGp.gerenciador.incommingMessage(6, "GP:LAUNCHER:SUCCESS_START_PROCESS:14:123:pikaloka:");
